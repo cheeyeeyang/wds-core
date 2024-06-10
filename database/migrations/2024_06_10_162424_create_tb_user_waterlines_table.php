cbd49fe32e85cc52137a2c10fc039f1b6b7b19b4
Cheeyeeyang Mouasue <mouasuecheeyeeyang@gmail.com>
2024-06-10 23:40:39 +0700

    this is new update for add model and migrates

---
+++ database/migrations/2024_06_10_162424_create_tb_user_waterlines_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_user_waterlines', function (Blueprint $table) {
+            $table->id();
+            $table->integer('employee_id')->unsigned()->nullable();
+            $table->foreign('employee_id')->references('id')->on('tb_employees')->onDelete('cascade');
+            $table->integer('waterline_id')->unsigned()->nullable();
+            $table->foreign('waterline_id')->references('id')->on('tb_water_lines')->onDelete('cascade');
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_user_waterlines');
+    }
+};
---
+++ database/migrations/2024_06_10_163140_create_tb_exports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_exports', function (Blueprint $table) {
+            $table->id();
+            $table->integer('employee_id')->unsigned()->nullable();
+            $table->foreign('employee_id')->references('id')->on('tb_employees')->onDelete('cascade');
+            $table->integer('branch_id')->unsigned()->nullable();
+            $table->foreign('branch_id')->references('id')->on('tb_branchs')->onDelete('cascade');
+            $table->integer('out_amount')->nullable();
+            $table->integer('int_amount')->nullable();
+            $table->integer('return_amount')->nullable();
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_exports');
+    }
+};
